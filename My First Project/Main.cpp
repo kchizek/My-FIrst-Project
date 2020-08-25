3920e648ee3e6b1007afe0ae9805de5df75321ba
Kamrin Chizek <69868407+kchizek@users.noreply.github.com>
2020-08-25 14:56:05 -0500

    Add project files.

---
+++ My First Project/Main.cpp
@@ -0,0 +1,16 @@
+/*
+	Kamrin Chizek
+	C++ Fall 2020
+	Due: August 31
+	Lab 1 My First Project
+	Creating my first program
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Blue World";
+
+	return 0;
+}
